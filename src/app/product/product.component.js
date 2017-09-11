c1c27a277959032ca2de24b6e021db65b03c3f93
Neil Kong <neildocs@gmail.com>
2017-09-10 23:22:43 -0400

    2017-09-10@11:20pm

--- src/app/product/product.component.js
+++ src/app/product/product.component.js
@@ -5,15 +5,38 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var repository_model_1 = require("../model/repository.model");
+var core_2 = require("@angular/core");
 var ProductComponent = (function () {
-    function ProductComponent() {
+    function ProductComponent(ref) {
         this.model = new repository_model_1.ModelRepository();
+        this.fontSizeWithUnits = "30px";
+        this.fontSizeWithoutUnits = "30";
     }
-    ProductComponent.prototype.getClasses = function () {
-        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    ProductComponent.prototype.getClasses = function (key) {
+        // return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+        var product = this.model.getProduct(key);
+        return "p-a-1 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    };
+    ProductComponent.prototype.getClassMap = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger": product.name == "Kayak",
+            "bg-info": product.price < 50
+        };
+    };
+    ProductComponent.prototype.getStyles = function (key) {
+        var product = this.model.getProduct(key);
+        return {
+            fontSize: "30px",
+            "margin.px": 100,
+            color: product.price > 50 ? "red" : "green"
+        };
     };
     return ProductComponent;
 }());
@@ -21,7 +44,8 @@ ProductComponent = __decorate([
     core_1.Component({
         selector: "app",
         templateUrl: "./product.template.html"
-    })
+    }),
+    __metadata("design:paramtypes", [core_2.ApplicationRef])
 ], ProductComponent);
 exports.ProductComponent = ProductComponent;
 //# sourceMappingURL=product.component.js.map
