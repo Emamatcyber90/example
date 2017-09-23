eab821059aba33a1d575eadc1e3bd7ba8832af59
Neil Kong <neildocs@gmail.com>
2017-09-23 13:44:09 -0400

    2017-09-23@1:45pm
    # Create Host Element Bindings
    #

--- src/app/directive/attr.directive.ts
+++ src/app/directive/attr.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, ElementRef, Attribute, Input, 
-         SimpleChange, Output, EventEmitter }    from "@angular/core";
+         SimpleChange, Output, EventEmitter, 
+         HostListener, HostBinding }    from "@angular/core";
 import { Product } from "../model/product.model";
 
 @Directive({
@@ -9,15 +10,16 @@ export class PaAttrDirective {
     // constructor(element: ElementRef, @Attribute("pa-attr")bgClass: string) {
     //     element.nativeElement.classList.add(bgClass || "bg-success");
     // }
-    constructor(private element: ElementRef) { 
-        this.element.nativeElement.addEventListener("click", e => {
-            if (null != this.product) {
-                this.click.emit(this.product.category);
-            }
-        });
-    }
+    // constructor(private element: ElementRef) { 
+    //     this.element.nativeElement.addEventListener("click", e => {
+    //         if (null != this.product) {
+    //             this.click.emit(this.product.category);
+    //         }
+    //     });
+    // }
 
     @Input("pa-attr")
+    @HostBinding("class")
     bgClass: string;
 
     @Input("pa-product")
@@ -26,18 +28,25 @@ export class PaAttrDirective {
     @Output("pa-category")
     click = new EventEmitter<string>();
 
-    ngOnInit() {
-        this.element.nativeElement.classList.add(this.bgClass || "bg-success");
+    @HostListener("click")
+    triggerCustomerEvent() {
+        if (null != this.product) {
+            this.click.emit(this.product.category);
+        }
     }
 
-    ngOnChanges(changes: {[property: string]: SimpleChange}) {
-        let change = changes["bgClass"];
-        let classList = this.element.nativeElement.classList;
-        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-            classList.remove(change.previousValue);
-        }
-        if (!classList.contains(change.currentValue)) {
-            classList.add(change.currentValue);
-        }
-    }
+    // ngOnInit() {
+    //     this.element.nativeElement.classList.add(this.bgClass || "bg-success");
+    // }
+
+    // ngOnChanges(changes: {[property: string]: SimpleChange}) {
+    //     let change = changes["bgClass"];
+    //     let classList = this.element.nativeElement.classList;
+    //     if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+    //         classList.remove(change.previousValue);
+    //     }
+    //     if (!classList.contains(change.currentValue)) {
+    //         classList.add(change.currentValue);
+    //     }
+    // }
 }
